f1c43dbf896875dce1430e8c46f1e51782a0ec0c
Shariful Islam <sharifahmadwd@gmail.com>
2026-01-09 22:36:00 +0600

    solve problem 10 ping pong challenge

---
+++ problem_9_factorial.js
@@ -0,0 +1,10 @@
+function calculateFactorial(num) {
+    let factorial = 1;
+    
+    for (let i = 1; i <= num; i++) {
+        factorial *= i; 
+    }
+    return factorial;
+}
+
+console.log(calculateFactorial(5));
