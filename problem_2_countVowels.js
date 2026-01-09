2e6a59f6dc6d6046bd8cd20b1081e8ceed443e9b
Shariful Islam <sharifahmadwd@gmail.com>
2026-01-09 21:58:29 +0600

    solve problem 2 count vowels in a string

---
+++ problem_2_countVowels.js
@@ -0,0 +1,14 @@
+function countVowels(str) {
+    let count = 0;
+    const vowels = "aeiou";
+    
+    for (let char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+console.log(countVowels("ProgrammingHero")); 
